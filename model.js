6b5e5d0526d70cc7118d6ddb91be6fe8b54f0ae5
Hunecke, Bastian <bastian.hunecke@lhind.dlh.de>
2018-01-09 20:13:28 +0100

    second iteration almost done

--- model.js
+++ model.js
@@ -1,37 +1,94 @@
 'use strict';
 let airports = require('./airports.json');
+let aircrafts = require('./ac_types.json');
 
-exports.determineStatus = () => {
-    let status = new Status(getDestination().airport_code, getDestination().airport_code, new Date(), getEnd());
-    console.log(JSON.stringify(status));
-    return status;
-};
-
-let getDestination = () => {
-    let randomIndex = Math.floor(Math.random() * airports.length);
-    return airports[randomIndex];
-};
-
-let getEnd = () => {
-    let now = new Date();
-    return new Date(now.getTime() + (Math.floor((Math.random() * 64800000) + 60000)));
+exports.determineStatus = aircraft => {
+    let statusList = [];
+    let predecessor = null;
+    for (let i = 0; i < 5; i++) {
+        let status = new Status(aircraft, predecessor);
+        statusList.push(status);
+        console.log(JSON.stringify(status));
+        predecessor = status;
+    }
+    return statusList;
 };
 
 class Status {
-    constructor(depAirport, arrAirport, start, end) {
-        this.aircraftId = "123498765";
+    constructor(aircraft, predecessor) {
+        if (predecessor !== null ) {
+            this.aircraftId = predecessor.aircraftId;
+        } else {
+            this.aircraftId = Math.floor((Math.random() * 899999999) + 100000000);
+        }
         this.detailName = "AircraftStatus";
         this.updatedTimestamp = 0;
         this.status = "AIRBORNE";
-        this.beginStatusTime = start;
-        this.endStatusTime = end;
-        this.beginStatusLocation = new Airport(depAirport);
-        this.endStatusLocation = new Airport(arrAirport);
-        this.currentFlightNo = "AV1234";
-        this.currentFlightNoIcao = "AVI1234";
+        let flightNo = Math.floor((Math.random() * 8999) + 1000);
+        this.currentFlightNo = "AV" + flightNo;
+        this.currentFlightNoIcao = "AVI" + flightNo;
         this.eventType = null;
         this.eventName = null;
+
+        this.determineAirports(predecessor, aircraft);
+        this.determineTimes(predecessor);
     }
+
+    determineAirports(predecessor, aircraft) {
+        let airportList = airports;
+        if (predecessor !== null) {
+            this.beginStatusLocation = predecessor.endStatusLocation;
+        } else {
+            let randomIndex = Math.floor(Math.random() * airports.length);
+            this.beginStatusLocation = new Airport(airportList[randomIndex].airport_code);
+        }
+        if (this.status === 'AIRBORNE') {
+            let depAirport = airportList.find(port => {
+                return port.airport_code === this.beginStatusLocation.code;
+            });
+            let indexToRemove = airportList.indexOf(depAirport);
+            airportList.splice(indexToRemove, 1);
+            while (this.endStatusLocation === undefined) {
+                let randomIndex = Math.floor(Math.random() * airports.length);
+                let airport = airportList[randomIndex];
+                if (this.validateFlightType(aircraft, depAirport, airport) === true) {
+                    this.endStatusLocation = new Airport(airport.airport_code);
+                }
+            }
+        } else {
+            this.endStatusLocation = new Airport(this.beginStatusLocation.code);
+        }
+    }
+
+    validateFlightType(aircraft, dep, arr) {
+        let routeType;
+        let distance = Math.floor(Math.sqrt((Math.pow((dep.longitude - arr.longitude), 2) + Math.pow((dep.latitude - arr.latitude), 2))));
+        if (distance > 50) {
+            routeType = "LONG_DISTANCE";
+        } else if (distance > 10) {
+            routeType = "MIDDLE_DISTANCE";
+        } else {
+            routeType = "SHORT_DISTANCE";
+        }
+        return (this.determineFlightType(aircraft) === routeType);
+    }
+
+
+    determineFlightType(aircraft) {
+        let flightType = aircrafts.find(ac => {
+            return ac.ac_type_name === aircraft;
+        });
+        return flightType.type;
+    };
+
+    determineTimes(predecessor) {
+        if (predecessor !== null) {
+            this.beginStatusTime = predecessor.endStatusTime;
+        } else {
+            this.beginStatusTime = new Date();
+        }
+        this.endStatusTime = new Date(this.beginStatusTime.getTime() + (Math.floor((Math.random() * 64800000) + 60000)));
+    };
 }
 
 class Airport {
