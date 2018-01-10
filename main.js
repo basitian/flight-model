cb070fce08070905d95a0cf5da05f407d0867912
Bastian Hunecke <basitian@web.de>
2018-01-10 12:51:48 +0100

    implemented rules for second iteration

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 'use strict';
 let model = require('./model.js');
 
-// Just for testing
-let airports = require('./airports');
 let acType = process.env.AC_TYPE || 'A320';
 
-let lax = airports.find(port => {
-   return port.airport_code === 'LAX';
-});
-let fra = airports.find(port => {
-    return port.airport_code === 'FRA';
-});
-let distance = Math.floor(Math.sqrt((Math.pow((lax.longitude - fra.longitude), 2) + Math.pow((lax.latitude - fra.latitude), 2))));
-console.log(distance);
-
-// Real shit!
-//model.determineStatus(acType);
+model.determineStatus(acType);
--- model.js
+++ model.js
@@ -48,10 +48,11 @@ class Status {
             });
             let indexToRemove = airportList.indexOf(depAirport);
             airportList.splice(indexToRemove, 1);
+            let acFlightType = this.determineFlightType(aircraft);
             while (this.endStatusLocation === undefined) {
                 let randomIndex = Math.floor(Math.random() * airports.length);
                 let airport = airportList[randomIndex];
-                if (this.validateFlightType(aircraft, depAirport, airport) === true) {
+                if (this.validateFlightType(acFlightType, depAirport, airport) === true) {
                     this.endStatusLocation = new Airport(airport.airport_code);
                 }
             }
@@ -60,19 +61,25 @@ class Status {
         }
     }
 
-    validateFlightType(aircraft, dep, arr) {
+    validateFlightType(acFlightType, dep, arr) {
+        return (acFlightType === this.determineDistance(dep, arr));
+    }
+
+
+    determineDistance(dep, arr) {
         let routeType;
-        let distance = Math.floor(Math.sqrt((Math.pow((dep.longitude - arr.longitude), 2) + Math.pow((dep.latitude - arr.latitude), 2))));
-        if (distance > 50) {
+        // let distance = Math.floor(Math.sqrt((Math.pow((dep.longitude - arr.longitude), 2) + Math.pow((dep.latitude - arr.latitude), 2))));
+        let distance = calculateDistance(dep.latitude, dep.longitude, arr.latitude, arr.longitude);
+        console.log(distance);
+        if (distance > 5000) {
             routeType = "LONG_DISTANCE";
-        } else if (distance > 10) {
+        } else if (distance > 2000) {
             routeType = "MIDDLE_DISTANCE";
         } else {
             routeType = "SHORT_DISTANCE";
         }
-        return (this.determineFlightType(aircraft) === routeType);
+        return routeType;
     }
-
 
     determineFlightType(aircraft) {
         let flightType = aircrafts.find(ac => {
@@ -90,6 +97,25 @@ class Status {
         this.endStatusTime = new Date(this.beginStatusTime.getTime() + (Math.floor((Math.random() * 64800000) + 60000)));
     };
 }
+
+let calculateDistance = (lat1, long1, lat2, long2) => {
+
+    const EARTH_RADIUS_KM = 6371.009;
+
+    let lat1Rad = degreesToRadians(lat1);
+    let long1Rad = degreesToRadians(long1);
+    let lat2Rad = degreesToRadians(lat2);
+    let long2Rad = degreesToRadians(long2);
+
+    let centralAngle = Math.abs(Math.atan(Math.sqrt(Math.pow(Math.cos(lat2Rad) * Math.sin(Math.abs(long1Rad - long2Rad)), 2) +
+        Math.pow((Math.cos(lat1Rad) * Math.sin(lat2Rad)) - (Math.sin(lat1Rad) * Math.cos(lat2Rad) * Math.cos(Math.abs(long1Rad - long2Rad))), 2)) /
+        ((Math.sin(lat1Rad) * Math.sin(lat2Rad)) + (Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.cos(Math.abs(long1Rad - long2Rad))))));
+    return centralAngle * EARTH_RADIUS_KM;
+};
+
+let degreesToRadians = (number) => {
+    return (number * (Math.PI / 180));
+};
 
 class Airport {
     constructor(code) {
